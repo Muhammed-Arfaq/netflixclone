03d83f609771f5f84f91fd7dc5a663714bb953d6
Muhammed Arfaq R V <arfaq20@gmail.com>
2022-12-13 17:08:18 +0530

    Netflic-Clone first commit

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react'
+import './Banner.css'
+import { API_KEY, imageUrl } from '../../Constants/Constants'
+import axios from '../../axios'
+
+function Banner() {
+    const [movie, setMovie] = useState()
+    useEffect(() => {
+        axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
+            const imgIndex = Math.floor(
+                Math.random() * response.data.results.length
+                );
+            console.log(response.data.results[imgIndex]);
+            setMovie(response.data.results[imgIndex])
+        })
+    }, [])
+    
+  return (
+    <div style={{backgroundImage: `url(${movie ? imageUrl+movie.backdrop_path : ""})`}} className='banner'>
+        <div className='content'>
+            <h1 className='title'>{movie ? (movie.original_name ? movie.original_name : movie.title) : ""}</h1>
+            <div className='banner_buttons'>
+                <button className='button'>Play</button>
+                <button className='button'>My list</button>
+            </div>
+            <h1 className='description'>{movie ? movie.overview : ""}</h1>
+        </div>
+        <div className="fade_bottom">
+
+        </div>
+    </div>
+  )
+}
+
+export default Banner
